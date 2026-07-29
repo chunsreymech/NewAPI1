a1acc4c49ae4109ac8ac503e8834dd348600f55e
devidsmith9527-crypto <soeurnglim@gmail.com>
2026-07-29 18:31:17 +0700

    Create Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewAPI1.Models
+{
+    public class Product
+    {
+        public int ProID{set;get;}
+        public string ProName{set;get;}
+        public decimal Price{set;get;}
+        public int Quantity{set;get;}
+    }
+}
